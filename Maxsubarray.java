3c38fcc3032aa8da24a9dade2112c297dc410e09
RanjithaSrinivasa <ranjithasrinivasa24@gmail.com>
2020-05-25 13:23:56 -0400

    Max subarray using Kadanes algorithm

--- Maxsubarray.java
+++ Maxsubarray.java
@@ -1,9 +1,18 @@
 class Solution { 
     public int maxSubArray(int[] nums) {
+
+    /* when input is [-1] assigning localsum
+       to zero gives wrong output */
+
     int currentsum = nums[0];
     int globalsum = nums[0];
-   // int currentsum = 0;
-    //int globalsum = (int)-Math.pow(2, 31) ;
+
+    /* method2 - assigning local sum to -2^31
+     * so that max is not 0
+     * int currentsum = 0;
+     * int globalsum = (int)-Math.pow(2, 31) ;
+     */
+
     for(int i = 1 ; i < nums.length; i++) {
         currentsum = Math.max(nums[i], currentsum + nums[i]);
         if(currentsum > globalsum) {
