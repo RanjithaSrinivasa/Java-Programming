d9a809a7b702f0a6b852bdd031c5156f8ac39d7e
RanjithaSrinivasa <ranjithasrinivasa24@gmail.com>
2020-06-23 02:26:57 -0400

    best time to buy and sell stocks

---
+++ Stocks.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        for(int i = 0 ; i< prices.length-1; i++ ){
+            for(int j = i+1; j < prices.length; j++){
+                if((prices[j] - prices[i]) > max)
+                    max  = prices[j] - prices[i];
+            }
+        }
+        return max;
+    }
+}
