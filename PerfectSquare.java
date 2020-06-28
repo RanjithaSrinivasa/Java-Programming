9eb33856bb2e2a3bf2ae5fc654ac8230add75c55
RanjithaSrinivasa <ranjithasrinivasa24@gmail.com>
2020-06-28 01:55:23 -0400

    checking if number is perfect square using binary search

---
+++ PerfectSquare.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long low = 1;
+        long high = num;
+        while(low <= high){
+            long mid = (low + high)/2;
+            if( mid*mid == num)
+                return true;
+            else if(mid * mid < num)
+                low = mid + 1;
+            else if(mid * mid > num)
+                high = mid - 1;
+        }
+        return false;        
+    }
+}
